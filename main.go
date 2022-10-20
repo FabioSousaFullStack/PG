b74c92eaced0d947c1f0ecdf2a54a21992d41045
Fabio Sousa <clubeteczagem@gmail.com>
2022-10-20 06:30:43 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println("")
+	fmt.Println("-----------------------------PROGRESSÃO GEOMÉTRICA-----------------------------------")
+
+	var primeiroTermo float64
+	fmt.Print("Digite o primeiro termo: ")
+	fmt.Scan(&primeiroTermo)
+
+	var razao float64
+	fmt.Print("Digite a razao: ")
+	fmt.Scan(&razao)
+
+	var numeroDeElementos float64
+	fmt.Print("Digite o número de elementos: ")
+	fmt.Scan(&numeroDeElementos)
+
+	for numeroDeElementos > 0 {
+		numeroDeElementos -= 1.
+
+		defer fmt.Println(primeiroTermo*(math.Pow(razao, numeroDeElementos)))
+	}   
+
+	
+}
